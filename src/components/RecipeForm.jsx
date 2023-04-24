32290c322e51a7f70b6655226b2fd0f31e14b5c8
Angela Palaszewski <delarosa.ang@gmail.com>
2023-04-24 11:09:34 -0600

    component skeletons

--- src/components/RecipeForm.jsx
+++ src/components/RecipeForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-function Home(){
+function RecipeForm() {
+  
         return (
             <div>
                 
@@ -8,4 +9,5 @@ function Home(){
         );
 }
 
-export default Home;
+
+export default RecipeForm;
