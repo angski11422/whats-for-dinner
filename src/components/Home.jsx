5d840f95fe946fc58b46f2cfc1ae10dc37b32223
Angela Palaszewski <delarosa.ang@gmail.com>
2023-04-24 17:17:38 -0600

    fixed async issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from 'react';
 import RandomRecipe from './RandomRecipe';
 
 function Home() {
-    const [randomRecipe, setRandomRecipe] = useState([])
+    const [randomRecipe, setRandomRecipe] = useState(null)
 
     useEffect(() => {
         fetch("http://www.themealdb.com/api/json/v1/1/random.php")
         .then((resp) => resp.json())
         .then(data => setRandomRecipe(data))
     }, [])
+
+    if (!randomRecipe){
+        return <h3>Loading...</h3>
+    }
 
     return (
         <div>
