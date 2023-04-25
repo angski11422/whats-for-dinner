61f64cb84c37ded9c665eb7b4b399cf0fde952e5
Angela Palaszewski <delarosa.ang@gmail.com>
2023-04-25 11:16:04 -0600

    created db.json

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import RecipeForm from './RecipeForm'
 
 function Search() {
+
+    // `https://www.themealdb.com/api/json/v1/1/search.php?f=${b}`
+    
     return (
-        <div className=" bg-gray-200">
+        <div>
             <div className="container h-screen flex justify-center items-center px-4 sm:px-6 lg:px-8">
                 <div className="relative">
                     <input type="text" className="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none" placeholder="Search anything..." />
@@ -21,3 +24,4 @@ function Search() {
 
 export default Search;
 
+// outer div className=" bg-gray-200"
